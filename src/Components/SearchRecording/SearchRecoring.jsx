4eda17cc95737c0146daec1cd5489d0c55eb9093
Mohamed Yahia <m.yahya199290@gmail.com>
2024-02-18 23:58:55 +0300

    creating recording for search

---
+++ src/Components/SearchRecording/SearchRecoring.jsx
@@ -0,0 +1,27 @@
+import CloseIcon from '@mui/icons-material/Close';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+
+const SearchRecoring = () => {
+  return (
+    <section className='search-recorder'>
+         <div className="recorder-container">
+            <header className="recorder-header">
+                <div className="cancel">
+                    <span><CloseIcon /></span>
+                </div>
+                <h3 className="listing">
+                    listing...
+                </h3>
+            </header>
+            <div className="recorder-box">
+                <div className="rec-mike">
+                    <span><KeyboardVoiceIcon /></span>
+                </div>
+                <p></p>
+            </div>
+         </div>
+    </section>
+  )
+}
+
+export default SearchRecoring
