27768fb441212433538a8a31bb2474e0c5a2b8af
Mohamed Yahia <m.yahya199290@gmail.com>
2024-02-22 20:09:38 +0300

    modifing global state context

--- src/Components/SearchRecording/SearchRecorder.jsx
+++ src/Components/SearchRecording/SearchRecorder.jsx
@@ -1,15 +1,25 @@
 import CloseIcon from '@mui/icons-material/Close';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
-
 import "./SearchRecorder.scss";
+import { useContext } from 'react';
+import { statesContext } from '../../Contexts/statesContext';
 
 const SearchRecorder = () => {
+
+    const {setIsRecording,theme} = useContext(statesContext);
   return (
-    <section className='search-recorder'>
-         <div className="recorder-container">
+    <section 
+        className='search-recorder' 
+        onClick={(e)=> {
+            if(e.target.classList.contains('search-recorder')){
+                setIsRecording(false)
+            }
+        }}
+        >
+         <div className={`${theme} recorder-container`}>
             <header className="recorder-header">
                 <div className="cancel">
-                    <span><CloseIcon /></span>
+                    <span onClick={()=> setIsRecording(false)}><CloseIcon /></span>
                 </div>
                 <h3 className="listing">
                     listing...
