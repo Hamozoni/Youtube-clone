eb96c8df6775cdc69c31695451a569dbd6725a90
Mohamed Yahia <m.yahya199290@gmail.com>
2024-03-07 19:32:03 +0300

    modifing the loading component

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,10 +1,26 @@
+import { useContext } from 'react';
 import './Loading.scss';
+import { statesContext } from '../../Contexts/statesContext';
+
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { language } from '../../Utils/language';
 
 const Loading = ()=> {
+
+    const {theme,lang} = useContext(statesContext)
     return (
         
         <div className="loading">
-            <div className="circle">
+            <div className={`${theme} logo`}>         
+                <div className='logo-cont' >
+                    <span className={`${theme} icon`}>
+                        <PlayArrowIcon />
+                        
+                    </span>
+                    <h1>
+                       { language[lang]?.logo }
+                    </h1>
+                </div>  
             </div>
         </div>
     );
