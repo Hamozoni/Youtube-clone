446077ecded9f9bd1d246cc5fa7a56e45e3d9257
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-07 14:38:02 +0300

    modified:   src/Pages/Channel/Channel.jsx

--- src/Pages/Channel/Channel.jsx
+++ src/Pages/Channel/Channel.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import './Channel.scss';
 
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
 import { fetchChannelApi } from '../../Utils/FetchApi';
 import { statesContext } from '../../Contexts/statesContext';
 
@@ -9,6 +11,8 @@ import ChannelAbout from '../../Components/ChannelAbout/ChannelAbout';
 import Error from '../../Components/Error/Error';
 import SideNavbarSmall from '../../Components/SideNavbar/SideNavSmall';
 import ChannelTaps from '../../Components/ChannelNavTaps/ChannelTaps';
+import { language } from '../../Utils/language';
+import ChannelSubscribeBtn from '../../Components/ChannelSubscibeBtn/ChannelSubscribeBtn';
 
 const ChanelDetails = ()=> {
 
@@ -26,7 +30,8 @@ const ChanelDetails = ()=> {
         setError(null);
         fetchChannelApi(`channel/about?id=${id}&lang=${lang}`)
         .then((data)=>{
-            setChanelDetail(data);  
+            setChanelDetail(data);
+            console.log(data); 
         })
         .catch((error)=> {
             setError(error)
@@ -49,11 +54,38 @@ const ChanelDetails = ()=> {
                         <img 
                             src={ chanelDetail?.banner[1]?.url || 
                                 chanelDetail?.banner[2]?.url} 
-                            alt="" 
+                            alt="banner" 
                         />
                     }
+
+                    <div className="ch-card">
+                        <img src={chanelDetail?.avatar[1]?.url} alt="avatar" />
+                        <div className="ch-info">
+                            <div className="ch-n">
+                                <span>
+                                     {chanelDetail?.channelHandle} {chanelDetail?.isVerified && <CheckCircleIcon /> }
+                                </span>
+                                <span>
+                                     {chanelDetail?.subscriberCountText + " " + language[lang].subscribers}
+                                </span>
+                                <span>
+                                     {chanelDetail?.videosCount + " " + language[lang].videos}
+                                </span>
+                            </div>
+                            <div className="ch-desc">
+                                <article>
+                                    {chanelDetail?.description?.slice(0,90) + "..."}
+                                </article>
+                                <CheckCircleIcon onClick={()=> setIsAboutChannelOpen(true)} /> 
+                            </div>
+                            <ChannelSubscribeBtn />
+                        </div>
+                    </div>
                 </div>
-                <ChannelTaps setIsAboutChannelOpen={setIsAboutChannelOpen}  />
+                <ChannelTaps 
+                    setIsAboutChannelOpen={setIsAboutChannelOpen}
+                    tabs={chanelDetail.tabs}
+                    />
                 <div className="channel-content">
                     { 
                         isAboutChannelOpen &&  
