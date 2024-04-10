08546a1856514079d9d48db3801021eb6c8262b4
Mohamed Yahia <m.yahya199290@gmail.com>
2024-04-10 17:24:20 +0300

    modified:   src/Components/SortsCard/ShortCard.jsx         modified:   src/Components/SortsCard/ShortsCard.scss

--- src/Components/SortsCard/ShortCard.jsx
+++ src/Components/SortsCard/ShortCard.jsx
@@ -5,35 +5,36 @@ import { useContext } from "react";
 import "./ShortsCard.scss";
 import ReactPlayer from "react-player";
 
-const ShortCard = ({ short, shorts }) => {
-  const { theme, setShorts, playingVideoId, setPlayingVideoId, dispatch } = useContext(statesContext);
+const ShortCard = ({ short}) => {
+  const { theme,  playingVideoId, setPlayingVideoId, dispatch } = useContext(statesContext);
 
   const navgate = useNavigate();
 
-  const clickHandler = () => {
-    setShorts(
-      shorts.map((short) => {
-        if ("thumbnail" in short) {
-          return short;
-        } else {
-          return;
-        }
-      }),
-    );
+  const clickHandler = (id) => {
     dispatch({ type: "add-to-history", payload: short });
-    navgate(`/shorts`);
+    navgate(`/shorts?id=${id}`);
   };
 
   return (
-    <div className="short-card" onClick={clickHandler} onMouseOver={() => setPlayingVideoId(short?.videoId)} onMouseLeave={() => setPlayingVideoId("")} onTouchEnd={() => setPlayingVideoId(short?.videoId)}>
+    <div 
+        className="short-card" 
+        onClick={()=>clickHandler(short?.videoId)} 
+        onMouseOver={() => setPlayingVideoId(short?.videoId)} 
+        onMouseLeave={() => setPlayingVideoId("")} 
+        onTouchEnd={() => setPlayingVideoId(short?.videoId)}
+        >
       <div className="short-img">
         {short?.thumbnail ? <img src={short?.thumbnail[0]?.url} alt={"channel"} /> : <div></div>}
         {playingVideoId === short?.videoId && <ReactPlayer className="short-player absolute" url={`hppts://www.youtube.com/watch?v=${short?.videoId}?autoPlay=1`} playing muted />}
         <span className="absolute short-layout"></span>
       </div>
-      <div className="short-desc">
-        <h4 className={`${theme} short-title`}>{short?.title?.length > 30 ? `${short?.title.slice(0, 30)}...` : short?.title}</h4>
-        <h6 className={`${theme} sh-viwes`}>{short?.viewCountText}</h6>
+      <div className='short-desc'>
+        <h4 className={`t-color-${theme} short-title`}>
+           {short?.title?.length > 30 ? `${short?.title.slice(0, 30)}...` : short?.title}
+        </h4>
+        <h6 className={`t-color-${theme}-3 sh-viwes`}>
+           {short?.viewCountText}
+        </h6>
       </div>
     </div>
   );
