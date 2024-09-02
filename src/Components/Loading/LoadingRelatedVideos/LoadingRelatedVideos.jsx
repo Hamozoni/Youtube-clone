82f07959c3ee274bccc7a557a16286396a667780
Mohamed Yahia <m.yahya199290@gmail.com>
2024-09-02 10:33:28 +0300

    modified:   src/Components/Loading/LoadingRelatedVideos/LoadingRelatedVideos.jsx         modified:   src/Components/Loading/LoadingRelatedVideos/LoadingRelatedVideos.scss         modified:   src/Components/VideoCard/VideoCard.scss         modified:   src/index.scss

--- src/Components/Loading/LoadingRelatedVideos/LoadingRelatedVideos.jsx
+++ src/Components/Loading/LoadingRelatedVideos/LoadingRelatedVideos.jsx
@@ -16,18 +16,22 @@ const LoadingRelatedVideos = ({ display }) => {
         </ul>
       </nav>
       <div className={`${display} loader-conyainer`}>
-        {cards.map((e) => (
-          <div className={`${display} loader-card`} key={e}>
-            <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2 loader-image loader-moviement`}></div>
-            <div className="loader-box ">
-              <div className={`back-color-${theme}-1  back-before-c-${theme}-2  image loader-moviement`}></div>
-              <div className="lod-t-box ">
-                <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2 chn-n loader-moviement`}></div>
-                <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2  laoder-ch-name loader-moviement`}></div>
+        {
+          cards.map((e) => (
+            
+            <div className={`${display} loader-card`} key={e}>
+              <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2 loader-image loader-moviement`}></div>
+              <div className="loader-box ">
+                <div className={`back-color-${theme}-1  back-before-c-${theme}-2  image loader-moviement`}></div>
+                <div className="lod-t-box ">
+                  <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2 chn-n loader-moviement`}></div>
+                  <div className={`back-color-${theme}-1 ${display}  back-before-c-${theme}-2  laoder-ch-name loader-moviement`}></div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+        
+          ))
+        }
       </div>
     </div>
   );
