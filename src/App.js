1eadab42a73cf7ae85e9cca185f45b75a8e2870c
Mohamed Yahia <m.yahya199290@gmail.com>
2024-09-23 13:00:59 +0300

    modified:   src/App.js         deleted:    src/Components/Loading/Loading.jsx         deleted:    src/Components/Loading/Loading.scss

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Layouts/Header/Header.jsx';
-import SideNavbar from './Models/SideNavbar/SideNavbar.jsx'
+import { useContext, useEffect, useState } from 'react';
+// pages
 import Home from './Pages/Home/Home';
 import Watch from './Pages/Watch/Watch';
 import SearchFeed from './Pages/SearchFeed/SearchFeed';
 import PlayList from './Pages/Playlist/Playlist.jsx';
-
-import {statesContext} from './Contexts/statesContext';
-import { useContext, useEffect, useState } from 'react';
 import Shorts from './Pages/Shorts/Shorts.jsx';
 import History from './Pages/History/History.jsx';
 import LikedVideos from './Pages/LikedVideos/LikedVideos.jsx';
-import SearchRecorder from './Models/SearchRecording/SearchRecorder.jsx';
-import Loading from './Components/Loading/Loading.jsx';
 import Channel from './Pages/Channel/Channel.jsx';
 import ChannelContent from './Pages/Channel/Outlets/ChannelContent/ChannelContent.jsx';
 import ChannelHome from "./Pages/Channel/Outlets/ChannelHome/ChannelHome.jsx"
 import Community from './Pages/Community/Community.jsx';
-
+// layouts
+import Header from './Layouts/Header/Header.jsx';
+// models
+import SearchRecorder from './Models/SearchRecording/SearchRecorder.jsx';
+import SideNavbar from './Models/SideNavbar/SideNavbar.jsx';
+// contexts
+import {statesContext} from './Contexts/statesContext';
 
 const App = ()=> {
 
   const {isSideNavbarOpen,isRecording } = useContext(statesContext);
-
-  const [isLoader,setLoader] = useState(true)
-
-
-  useEffect(()=>{
-
-  const timeOut = setTimeout(()=>{
-      setLoader(false);
-    },3000);
-
-    return ()=> clearTimeout(timeOut)
-  },[setLoader])
-
 
   return (
       < BrowserRouter >
@@ -46,10 +34,6 @@ const App = ()=> {
           {
             isRecording && 
             <SearchRecorder />
-          }
-          {
-            isLoader && 
-            <Loading />
           }
         <Routes>
           <Route path='/' exact element={<Home />} />
